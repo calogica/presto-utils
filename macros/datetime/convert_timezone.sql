e7fc3e78843c7610ab97b7af8b19ed61a71f44b8
clausherther <claus@calogica.com>
2021-05-05 11:19:20 -0600

    change data type on timezone converter

--- macros/datetime/convert_timezone.sql
+++ macros/datetime/convert_timezone.sql
@@ -1,3 +1,3 @@
 {% macro presto__convert_timezone(column, target_tz, source_tz) -%}
-cast({{ column }} at time zone '{{ target_tz }}' as {{ dbt_utils.type_timestamp() }})
+cast({{ column }} as {{ dbt_utils.type_timestamp() }}) at time zone '{{ target_tz }}'
 {%- endmacro -%}
